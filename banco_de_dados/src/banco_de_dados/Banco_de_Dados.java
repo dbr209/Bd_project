a6fcbac91d61e375589bb82eb2caa32a9247f950
Vilmar Filho <162987159+dbr209@users.noreply.github.com>
2025-03-01 09:13:30 -0300

    Update Banco_de_Dados.java

--- banco_de_dados/src/banco_de_dados/Banco_de_Dados.java
+++ banco_de_dados/src/banco_de_dados/Banco_de_Dados.java
@@ -1,9 +1,12 @@
 package banco_de_dados;
 
+import java.io.IOException;
+
 public class Banco_de_Dados {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Get_Infos get = new Get_Infos();
-		String dt = get.get_bdroot();
-		System.out.print(dt);
+		String dt[] = get.get_bdroot();
+		
+		System.out.print(dt[1]);
 	}
 }
