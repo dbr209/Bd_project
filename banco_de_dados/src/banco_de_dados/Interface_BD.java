71c44b4661b6218a9be3ae184b592cf17ec8626b
Vilmar Filho <162987159+dbr209@users.noreply.github.com>
2025-03-06 09:07:10 -0300

    Update Interface_BD.java

--- banco_de_dados/src/banco_de_dados/Interface_BD.java
+++ banco_de_dados/src/banco_de_dados/Interface_BD.java
@@ -1,32 +1,43 @@
 package banco_de_dados;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Interface_BD {
-	public static void main() {
+	public static void main(String[] args) throws IOException{
 		
 		System.out.println("===== Banco de Dados =====");
-		System.out.print("1. Criar Database\n2.Excluir Database\n3.Acessar Database\n4. Listar Databases\n0. Sair\n\n-> ");
 		
 		Scanner leitor = new Scanner(System.in);
-		int op = leitor.nextInt();
 		
-		switch(op) {
-			case 1:
-				
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			default:
-				leitor.close();
-				main();
+		Banco_de_Dados bd = new Banco_de_Dados();
+		bd.inicializar();
 		
-		}
-		
+		boolean sair = false;
+		do {
+			System.out.print("1. Criar Database\n2. Excluir Database\n3. Acessar Database\n4. Listar Databases\n0. Sair\n\n-> ");
+			int op = leitor.nextInt();
+			
+			switch(op) {
+				case 1:
+					System.out.print(false);
+					break;
+				case 2:
+					System.out.print(false);
+					break;
+				case 3:
+					System.out.print(false);
+					break;
+				case 4:
+					bd.listar_databases();
+					break;
+				case 0:
+					sair = true;
+					break;
+				default:
+					leitor.close();
+			}
+		} while(sair == false);
 		
 		leitor.close();
 	}
