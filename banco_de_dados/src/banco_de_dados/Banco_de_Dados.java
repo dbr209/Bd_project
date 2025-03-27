13ee449a3c4597a885d417037219599423270653
Vilmar Filho <162987159+dbr209@users.noreply.github.com>
2025-03-27 10:18:52 -0300

    Update Banco_de_Dados.java

--- banco_de_dados/src/banco_de_dados/Banco_de_Dados.java
+++ banco_de_dados/src/banco_de_dados/Banco_de_Dados.java
@@ -24,12 +24,13 @@ public class Banco_de_Dados {
 			System.out.println("IOException");
 		}
 	}
-	public void excluir_database(Scanner sc) {
-        System.out.println("Qual arquivo você deseja excluir: "); //Pergunta ao usuário qual arquivo deseja excluir
-        String caminho = sc.nextLine(); //Lê o arquivo
+	public void excluir_database() {
+		
+        System.out.println("Qual Banco de Dados você deseja excluir: "); //Pergunta ao usuário qual arquivo deseja excluir
+        String bdExcluir = sc.nextLine(); //Lê o arquivo
 
-        File arquivo = new File(caminho); //Instanciona um objeto do arquivo lido
-
+        File arquivo = new File(arquivo_path + bdExcluir); //Instanciona um objeto do arquivo lido
+        
         try {
             if (!arquivo.exists()) { // Verifica se o arquivo não existe
                 System.out.println("Não existe nenhum arquivo com esse nome.");
@@ -37,7 +38,13 @@ public class Banco_de_Dados {
             }
             System.out.println("Informe a senha do arquivo: "); //Se o arquivo existir, pergunta a senha para exclusão
             String senha = sc.nextLine();
-
+            
+            int i = 0;
+            while(!bd_info[i].equals(bdExcluir)) {
+            	i += 1;
+            }
+            
+            String password = bd_info[i+1];
             if (senha.equals(password)) { // Verifica se a senha é igual a informada
                 if (arquivo.delete()) { // Tenta excluir o arquivo
                     System.out.println("Arquivo excluído com sucesso.");
@@ -47,25 +54,36 @@ public class Banco_de_Dados {
             } else {
                 System.out.println("Senha incorreta. O arquivo não foi excluído."); //Se a senha inserida for incorreta imprime isso
             }
+            
+            // Alterar o bd_info
+            
+            String newBdInfo[] = new String[bd_info.length - 2];
+            Path path = Path.of(arquivo_path + "bd_info");
+            
+            int aux = 0;
+            for(int j = 0;j < bd_info.length; j++) {
+            	if(j != i && j != i + 1) {
+            		newBdInfo[aux] = bd_info[j];
+            		aux += 1;
+            	}
+            }
+            
+            newBdInfo[0] = "" + (Integer.parseInt(newBdInfo[0]) - 1);
+            bd_info = newBdInfo;
+
+            for(int k = 0; k < bd_info.length; k++) {
+            	if(k == 0) {
+            		Files.writeString(path, newBdInfo[k] + "\n");
+            	} else {
+            		Files.writeString(path, newBdInfo[k] + "\n", StandardOpenOption.APPEND);
+            	}
+            }
+            
         } catch (Exception e) {
             System.out.println(-1); // Trata exceções
-        } 
-    }
-	public void listar_databases() {
-		System.out.println("");
-		
-		if(bd_info[0].equals("-2")) {
-			System.out.println("No Databases Available");
-		}
-		else {
-			int bd_total = (Integer.parseInt(bd_info[0]) * 2);
-			for(int i = 1; i <= bd_total; i = i + 2) {
-				System.out.println(bd_info[i]);
-			}
-		System.out.println("");
-		}
+            
+        }
 	}
-}
 
 	public void createDB() throws Exception {
 		
@@ -192,3 +210,4 @@ public class Banco_de_Dados {
 		}
 	sc.close();
 	}
+}
