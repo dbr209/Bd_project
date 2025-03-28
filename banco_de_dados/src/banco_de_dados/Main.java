c7e3da8b0b4953cc90005cba2dffe44182d2af2b
Vilmar Filho <162987159+dbr209@users.noreply.github.com>
2025-03-28 09:03:58 -0300

    Create Main.java

---
+++ banco_de_dados/src/banco_de_dados/Main.java
@@ -0,0 +1,10 @@
+package banco_de_dados;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException, Exception{
+		Interface_BD banco_de_dados = new Interface_BD();
+		banco_de_dados.interfacebd();
+	}
+}
