f9dcf29ef28410fef1026d4cd13b8c71c93299d0
Vilmar Filho <162987159+dbr209@users.noreply.github.com>
2025-04-05 16:41:27 -0300

    Update Infos_Config.java

--- banco_de_dados/src/banco_de_dados/Infos_Config.java
+++ banco_de_dados/src/banco_de_dados/Infos_Config.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Infos_Config{
+public class Infos_Config {
 	public String[] get_bdroot() throws IOException {
 		String arquivo_path = "src\\banco_de_dados\\bd_root\\bd_info";
 
@@ -18,7 +18,7 @@ public class Infos_Config{
 			
 			if(quant_databases.equals("0")) { // Caso o numero lido for "0" ele retorna pois não tem Databases
 				leitor.close();
-				return new String[]{"-2"}; // NoDatabaseAvailable
+				return new String[]{"0"}; // NoDatabaseAvailable
 			}
 			
 			int	quant_lines = (int) Files.lines(Paths.get("src", "banco_de_dados", "bd_root", "bd_info")).count(); // Puxa a quantidade de linhas
@@ -40,5 +40,31 @@ public class Infos_Config{
 		catch(FileNotFoundException e){
 			return new String[]{"-1"}; // FileNotFound;
 		}
+	}
+	
+	public String[] get_tabela_info(String nomeDB) throws IOException{
+		String arquivo_path = "src\\banco_de_dados\\bd_root\\" + nomeDB + "\\tabela_info.txt";
+			File aqTBL = new File(arquivo_path); // Abre o arquivo
+			Scanner leitor = new Scanner(aqTBL); // Escaneia o arquivo
+			String quant_databases = leitor.nextLine(); // Lê a primeira linha
+			
+			if(quant_databases.equals("0")) { // Caso o numero lido for "0" ele retorna pois não tem Databases
+				leitor.close();
+				return new String[]{"0"}; // NoTableAvailable
+			}
+			int	quant_lines = (int) Files.lines(Paths.get(arquivo_path)).count(); // Puxa a quantidade de linhas
+			
+			String data[] = new String[quant_lines]; // Cria o array 
+			data[0] = quant_databases;
+			
+			// Lê as linhas restantes e salva no array
+			int i = 1;
+			while(leitor.hasNext()) {
+				data[i] = leitor.nextLine();
+				i++;
+			}
+
+			leitor.close();			
+			return data; // Retorna o array}
 	}
 }
