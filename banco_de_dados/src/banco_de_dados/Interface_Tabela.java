08f9d7757fc4316a21fedf6b8e671833a11ee57b
Vilmar Filho <162987159+dbr209@users.noreply.github.com>
2025-04-05 16:42:19 -0300

    Update Interface_Tabela.java

--- banco_de_dados/src/banco_de_dados/Interface_Tabela.java
+++ banco_de_dados/src/banco_de_dados/Interface_Tabela.java
@@ -1,10 +1,38 @@
 package banco_de_dados;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Interface_Tabela {
-	public static void main() {
-		Scanner leitor = new Scanner(System.in);
-		leitor.close();
+	
+	public void interfaceTBL(String nameDB, Scanner sc) throws IOException {
+		
+		Tabela tbl = new Tabela();
+		tbl.inicializar(nameDB, sc);
+		
+		boolean sair = false;
+		do {
+			System.out.println("========== TABELAS ==========");
+			tbl.listarTables();
+			System.out.print("1. Criar Tabela\n2. Excluir Tabela\n3. Acessar Tabela\n0. Sair\n\n-> ");
+			String op = sc.nextLine();
+			
+			switch(op) {
+				case "1":
+					tbl.createTable();
+					break;
+				case "2":
+					tbl.excluirTable();
+					break;
+				case "3":
+					tbl.acessarTable();
+					break;
+				case "0":
+					sair = true;
+					break;
+				default:
+					System.out.println("Opcao Invalida");
+			}
+		} while(sair == false);
 	}
 }
